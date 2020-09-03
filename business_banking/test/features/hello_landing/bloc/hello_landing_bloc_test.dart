62175309be6057304fd5f1f1f2c24ee9d44cbb77
Max Walters <max.walters10@gmail.com>
2020-09-03 09:57:05 -0400

    Commit bloc test

---
+++ business_banking/test/features/hello_landing/bloc/hello_landing_bloc_test.dart
@@ -0,0 +1,7 @@
+import 'package:business_banking/features/hello_landing/bloc/hello_landing_bloc.dart';
+import 'package:business_banking/features/hello_landing/model/hello_landing_view_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  
+}
