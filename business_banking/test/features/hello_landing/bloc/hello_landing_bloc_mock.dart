ae83abf8dc2aa54f90e1eb5373d46ba2080bde83
Max Walters <max.walters10@gmail.com>
2020-09-03 09:55:21 -0400

    bloc mock

---
+++ business_banking/test/features/hello_landing/bloc/hello_landing_bloc_mock.dart
@@ -0,0 +1,15 @@
+import 'package:clean_framework/clean_framework.dart';
+import 'package:business_banking/features/hello_landing/bloc/hello_landing_bloc.dart';
+import 'package:business_banking/features/hello_landing/model/hello_landing_view_model.dart';
+import 'package:mockito/mockito.dart';
+
+class HelloLandingBlocMock extends Fake implements HelloLandingBloc {
+  @override
+  Pipe<HelloLandingViewModel> helloLandingViewModelPipe = Pipe<HelloLandingViewModel>();
+
+  HelloLandingBlocMock() {
+    helloLandingViewModelPipe.onListen(() {
+      helloLandingViewModelPipe.send(HelloLandingViewModel());
+    });
+  }
+}
