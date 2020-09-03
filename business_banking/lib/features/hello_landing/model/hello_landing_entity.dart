92e1ecbc490b94bbca58d0100b7d7dba533b400b
Max Walters <max.walters10@gmail.com>
2020-09-03 16:48:35 -0400

    Update model files for task change

--- business_banking/lib/features/hello_landing/model/hello_landing_entity.dart
+++ business_banking/lib/features/hello_landing/model/hello_landing_entity.dart
@@ -1,15 +1,31 @@
 import 'package:clean_framework/clean_framework.dart';
 
 class HelloLandingEntity extends Entity {
+  final String name;
+  final int lastFour;
+  final double balance;
+  
   HelloLandingEntity(
-    {List<EntityError> errors = const []}
-  ) : super(errors: errors);
+    {List<EntityError> errors = const [],
+     String name,
+     int lastFour,
+     this.balance = 0.0})
+    : name = name ?? '',
+      lastFour = lastFour ?? 0000,
+      super(errors: errors);
   
   @override
-  List<Object> get props => [errors];
+  List<Object> get props => [
+    errors, name, lastFour, balance
+  ];
   
   @override
-  merge({errors}) {
-    return HelloLandingEntity(errors: errors ?? this.errors);
+  merge({errors, String name, int lastFour, double balance}) {
+    return HelloLandingEntity(
+      errors: errors ?? this.errors,
+      name: name ?? this.name,
+      lastFour: lastFour ?? this.lastFour,
+      balance: balance ?? this.balance
+    );
   }
 }
--- business_banking/lib/features/hello_landing/model/hello_landing_view_model.dart
+++ business_banking/lib/features/hello_landing/model/hello_landing_view_model.dart
@@ -1,5 +1,15 @@
 import 'package:clean_framework/clean_framework.dart';
 
 class HelloLandingViewModel extends ViewModel {
-  HelloLandingViewModel();
+  final String accountName;
+  final int accountLastFour;
+  final double accountBalance;
+  
+  HelloLandingViewModel({
+    this.accountName,
+    this.accountLastFour,
+    this.accountBalance
+  }) : assert(accountName != null &&
+              accountLastFour != null &&
+              accountBalance != null);
 }
