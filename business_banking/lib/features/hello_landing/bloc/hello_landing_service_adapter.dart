227a4b146bc06ca0c118b459a3345313c3320349
Max Walters <max.walters10@gmail.com>
2020-09-03 14:17:19 -0400

    Fix service adapter

--- business_banking/lib/features/hello_landing/bloc/hello_landing_service_adapter.dart
+++ business_banking/lib/features/hello_landing/bloc/hello_landing_service_adapter.dart
@@ -1,15 +1,16 @@
 import 'package:clean_framework/clean_framework.dart';
 import 'package:business_banking/features/hello_landing/model/hello_landing_entity.dart';
 import 'package:business_banking/features/hello_landing/api/hello_landing_service.dart';
+import 'package:business_banking/features/hello_landing/api/hello_landing_service_response_model.dart';
 import 'package:clean_framework/clean_framework_defaults.dart';
 
 class HelloLandingServiceAdapter extends ServiceAdapter<HelloLandingEntity,
-    JsonRequestModel, JsonResponseModel, HelloLandingService> {
+    JsonRequestModel, HelloLandingServiceResponseModel, HelloLandingService> {
   HelloLandingServiceAdapter() : super(HelloLandingService());
 
   @override
   HelloLandingEntity createEntity(
-      HelloLandingEntity initialEntity, JsonResponseModel responseModel) {
+      HelloLandingEntity initialEntity, HelloLandingServiceResponseModel responseModel) {
     return initialEntity.merge(errors: <EntityError>[]);
   }
 }
