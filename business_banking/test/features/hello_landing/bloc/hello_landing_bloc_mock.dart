471592c7ffc178c3a22a381831853cccdd220643
Max Walters <max.walters10@gmail.com>
2020-09-03 10:25:43 -0400

    forgot dispose

--- business_banking/test/features/hello_landing/bloc/hello_landing_bloc_mock.dart
+++ business_banking/test/features/hello_landing/bloc/hello_landing_bloc_mock.dart
@@ -12,4 +12,7 @@ class HelloLandingBlocMock extends Fake implements HelloLandingBloc {
       helloLandingViewModelPipe.send(HelloLandingViewModel());
     });
   }
+  
+  @override
+  void dispose() {}
 }
