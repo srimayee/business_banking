17e7892f3cf65f138df4092ae29163dad777beb1
Max Walters <max.walters10@gmail.com>
2020-09-03 09:32:56 -0400

    Adding in temporary locator

--- business_banking/lib/features/hello_landing/api/hello_landing_service.dart
+++ business_banking/lib/features/hello_landing/api/hello_landing_service.dart
@@ -1,5 +1,6 @@
 import 'package:clean_framework/clean_framework.dart';
 import 'package:clean_framework/clean_framework_defaults.dart';
+import 'package:business_banking/example_locator.dart';
 
 class HelloLandingService extends EitherService<JsonRequestModel, JsonResponseModel> {
   HelloLandingService() : super(
@@ -12,4 +13,7 @@ class HelloLandingService extends EitherService<JsonRequestModel, JsonResponseMo
   JsonResponseModel parseResponse(Map<String, dynamic> jsonResponse) {
     return JsonResponseModel.fromJson(jsonResponse);
   }
+  
+  // Getting error with JsonResponseModel.fromJson(jsonResponse), so I'm going to write a temporary one
+  
 }
--- business_banking/lib/features/hello_landing/bloc/hello_landing_usecase.dart
+++ business_banking/lib/features/hello_landing/bloc/hello_landing_usecase.dart
@@ -2,6 +2,7 @@ import 'package:clean_framework/clean_framework.dart';
 import 'package:business_banking/features/hello_landing/model/hello_landing_view_model.dart';
 import 'package:business_banking/features/hello_landing/model/hello_landing_entity.dart';
 import 'package:business_banking/features/hello_landing/bloc/hello_landing_service_adapter.dart';
+import 'package:business_banking/example_locator.dart';
 
 class HelloLandingUseCase extends UseCase {
   Function(ViewModel) _viewModelCallBack;
