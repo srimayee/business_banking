fbee6754e50eb4a374b6b61edb07dc64bd9b0af6
Max Walters <max.walters10@gmail.com>
2020-09-08 09:54:34 -0400

    Remove debug banner and adjust system colors

--- business_banking/lib/main.dart
+++ business_banking/lib/main.dart
@@ -2,15 +2,23 @@ import 'package:clean_framework/clean_framework.dart';
 import 'package:business_banking/features/cash_accounts/ui/cash_accounts_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
 
 import 'example_locator.dart';
 
 void main() {
   logger().setLogLevel(LogLevel.verbose);
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.green,
+      statusBarColor: Colors.green
+    )
+  );
 
   runApp(
     MaterialApp(
       home: CashAccountsWidget(),
+      debugShowCheckedModeBanner: false,
     ),
   );
 }
