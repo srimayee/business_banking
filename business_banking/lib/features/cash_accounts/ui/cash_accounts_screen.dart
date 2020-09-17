72ca22c389cdf926501db2ff149b926bc2f37989
Amr Elshamy <elshaa1@nationwide.com>
2020-09-17 09:52:34 -0400

    add some formatting

--- business_banking/lib/features/cash_accounts/ui/cash_accounts_screen.dart
+++ business_banking/lib/features/cash_accounts/ui/cash_accounts_screen.dart
@@ -1,45 +1,34 @@
 import 'dart:ui';
-import 'package:clean_framework/clean_framework.dart';
-import 'package:business_banking/features/cash_accounts/model/cash_accounts_view_model.dart';
-import 'package:flutter/material.dart';
+
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:business_banking/features/cash_accounts/model/cash_accounts_view_model.dart';
+import 'package:clean_framework/clean_framework.dart';
+import 'package:flutter/material.dart';
 
 class CashAccountsScreen extends Screen {
   final CashAccountsViewModel viewModel;
   final VoidCallback navigateToAccountDetail;
 
-  CashAccountsScreen(
-      {@required this.viewModel, @required this.navigateToAccountDetail})
+  CashAccountsScreen({@required this.viewModel, @required this.navigateToAccountDetail})
       : assert(() {
           return viewModel != null;
         }());
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: Text(
-            'Business Banking',
-            key: Key('CAappBarName')
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        GestureDetector(
+          child: AccountCard(
+            viewModel: viewModel,
+            key: Key('accountCard1'),
           ),
-        ),
-        backgroundColor: Colors.grey[300],
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              GestureDetector(
-                child: AccountCard(viewModel: viewModel, key: Key('accountCard1'),),
-                onTap: () {
-                  navigateToAccountDetail();
-                },
-              )
-            ],
-          ),
-        ),
-      ),
+          onTap: () {
+            navigateToAccountDetail();
+          },
+        )
+      ],
     );
   }
 }
@@ -90,8 +79,7 @@ class AccountCard extends StatelessWidget {
                     ),
                     Text(
                       '\$' + viewModel.balance.toStringAsFixed(2),
-                      style: TextStyle(
-                          fontSize: 25.0, fontWeight: FontWeight.w300),
+                      style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w300),
                       key: Key('balance'),
                     )
                   ],
--- business_banking/lib/features/cash_accounts/ui/cash_accounts_widget.dart
+++ business_banking/lib/features/cash_accounts/ui/cash_accounts_widget.dart
@@ -1,6 +1,7 @@
-import 'package:clean_framework/clean_framework.dart';
 import 'package:business_banking/features/cash_accounts/bloc/cash_accounts_bloc.dart';
+import 'package:clean_framework/clean_framework.dart';
 import 'package:flutter/material.dart';
+
 import 'cash_accounts_presenter.dart';
 
 class CashAccountsWidget extends StatelessWidget {
