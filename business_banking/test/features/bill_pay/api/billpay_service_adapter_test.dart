3ce2c31ae67071ee9c1e56c688fb2f6aa9a54f1a
Vipin <vipinvijayan23@gmail.com>
2020-09-20 21:58:55 -0400

    Service adapter test clean up

--- business_banking/test/features/bill_pay/api/billpay_service_adapter_test.dart
+++ business_banking/test/features/bill_pay/api/billpay_service_adapter_test.dart
@@ -1,6 +1,3 @@
-import 'package:business_banking/features/account_detail/api/account_detail_service_response_model.dart';
-import 'package:business_banking/features/account_detail/bloc/account_detail_service_adapter.dart';
-import 'package:business_banking/features/account_detail/model/account_detail_entity.dart';
 import 'package:business_banking/features/billpay/api/billpay_service_response_model.dart';
 import 'package:business_banking/features/billpay/bloc/billpay_service_adapter.dart';
 import 'package:business_banking/features/billpay/model/billpay_entity.dart';
