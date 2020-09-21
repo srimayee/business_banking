b7ea40bf9de548add80ae262f9042df1febca2c7
Vipin <vipinvijayan23@gmail.com>
2020-09-20 21:53:17 -0400

    App Test reverted and added Bill pay test  methods

--- business_banking/test_driver/app_test.dart
+++ business_banking/test_driver/app_test.dart
@@ -5,6 +5,13 @@ void main() {
   group('Banking App Driver Tests:', () {
     final signInText = find.byValueKey('signInText');
     final loginButton = find.byValueKey('login_button_key');
+    final accountCard = find.byValueKey('accountCard1');
+    final cashAccountBalance = find.byValueKey('balance');
+    final cashAccountAppBar = find.byValueKey('CAappBarName');
+    final backButton = find.byValueKey('backButton');
+    final accountDetailAppBar = find.byValueKey('ADappBarL4');
+    final bigBalance = find.byValueKey('bigBalance');
+    final depHold = find.byValueKey('depHold');
     final btnBillPay = find.byValueKey('PayBtn');
     final billPayAppBar = find.byValueKey('CBillPayAppBarTitle');
     final btnOK = find.byValueKey('BtnOK');
@@ -21,9 +28,42 @@ void main() {
       }
     });
 
+    // These will only pass with the specific JSON included in the Mockoon folder
+
     test('LoginScreen, screen is displayed', () async {
       expect(await driver.getText(signInText), 'Sign In');
     });
+
+    test('CashAccountsScreen, navigated to and app bar is displayed', () async {
+      await driver.tap(loginButton);
+      await driver.waitForAbsent(loginButton);
+      expect(await driver.getText(cashAccountAppBar), 'Business Banking');
+    });
+
+    test('CashAccountsScreen, account balance is displayed on card', () async {
+      expect(await driver.getText(cashAccountBalance), '\$3545.54');
+    });
+
+    test('AccountDetailScreen, app bar is displayed', () async {
+      await driver.tap(accountCard);
+      expect(await driver.getText(accountDetailAppBar), '*6542');
+    });
+
+    test('AccountDetailScreen, account balance is displayed', () async {
+      expect(await driver.getText(bigBalance), '\$3545.54');
+    });
+
+    test('AccountDetailScreen, deposit hold ammount is displayed on card',
+        () async {
+      expect(await driver.getText(depHold), '\$0.00');
+    });
+
+    test('CashAccountScreen, check to find correct app bar', () async {
+      await driver.tap(backButton);
+      await driver.waitForAbsent(accountDetailAppBar);
+      expect(await driver.getText(cashAccountAppBar), 'Business Banking');
+    });
+
     test('Bill Pay Screen, navigated to and app bar is displayed', () async {
       await driver.tap(loginButton);
       await driver.waitForAbsent(loginButton);
