54484eee9911c9009660a78e0a74dcf257eec52f
Vipin <vipinvijayan23@gmail.com>
2020-09-20 21:48:54 -0400

    Bill Pay adapter test Update.

--- business_banking/test/features/bill_pay/api/billpay_service_adapter_test.dart
+++ business_banking/test/features/bill_pay/api/billpay_service_adapter_test.dart
@@ -1,32 +1,19 @@
 import 'package:business_banking/features/account_detail/api/account_detail_service_response_model.dart';
 import 'package:business_banking/features/account_detail/bloc/account_detail_service_adapter.dart';
 import 'package:business_banking/features/account_detail/model/account_detail_entity.dart';
+import 'package:business_banking/features/billpay/api/billpay_service_response_model.dart';
+import 'package:business_banking/features/billpay/bloc/billpay_service_adapter.dart';
+import 'package:business_banking/features/billpay/model/billpay_entity.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('Entity is created by service adapter', () {
-    final entity = AccountDetailServiceAdapter().createEntity(
-        AccountDetailEntity(),
-        AccountDetailServiceResponseModel.fromJson({
-          'name': 'Employee Checking',
-          'lastFour': '6542',
-          'accountType': 'Employee Checking',
-          'routingNum': 746395735,
-          'balance': 3545.54,
-          'begBalance': 3674.43,
-          'penTrans': -128.89,
-          'depHolds': 0.00
+    final entity = BillPayServiceAdapter().createEntity(
+        BillPayEntity(),
+        BillPayServiceResponseModel.fromJson({
+          'amount': 0,
         }));
 
-    expect(entity, AccountDetailEntity(
-      name: 'Employee Checking',
-      lastFour: '6542',
-      accountType: 'Employee Checking',
-      routingNumber: 746395735,
-      balance: 3545.54,
-      beginningBalance: 3674.43,
-      pendingTransactions: -128.89,
-      depositHolds: 0.00
-    ));
+    expect(entity, BillPayEntity(amount: 0));
   });
 }
