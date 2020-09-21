bcd19bec2b918c64836d2f10715e5f4c2f63bb3b
Amr Elshamy <elshaa1@nationwide.com>
2020-09-21 10:51:41 -0400

    fix typo

--- business_banking/test/features/promos/ui/promos_ui_test.dart
+++ business_banking/test/features/promos/ui/promos_ui_test.dart
@@ -8,7 +8,7 @@ import '../../../util/image_test_util.dart';
 void main() {
   testWidgets('Promos widget has the proper elements', (tester) async {
     //In widget tests, the default HTTP client has been replaced with one that always returns 400s.
-    //user this utility to return dummy image for testing
+    //use this utility to return dummy image for testing
     //https://github.com/flutter/flutter_markdown/blob/a2fa53b7a29dc58b6f26b53e23a6f5a8d283dd66/test/flutter_markdown_test.dart#L506-L548
     provideMockedNetworkImages(() async {
       final testWidget = MaterialApp(
