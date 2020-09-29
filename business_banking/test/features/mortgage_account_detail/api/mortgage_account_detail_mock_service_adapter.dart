aab5f1a2f40226bafc64ab22ac98ad793a7a4fdc
sksuresh <sureshdokala@gmail.com>
2020-09-29 09:26:19 -0400

    Mortgage: created mock test for MortgageAccountDetailMockServiceAdapter

---
+++ business_banking/test/features/mortgage_account_detail/api/mortgage_account_detail_mock_service_adapter.dart
@@ -0,0 +1,25 @@
+
+import 'package:business_banking/features/mortgage_account_details/api/mortgage_account_detail_service_response_model.dart';
+import 'package:business_banking/features/mortgage_account_details/bloc/mortgage_account_detail_service_adapter.dart';
+import 'package:business_banking/features/mortgage_account_details/model/mortgage_account_detail_entity.dart';
+import 'package:mockito/mockito.dart';
+
+class MortgageAccountDetailMockServiceAdapter extends Fake implements MortgageAccountDetailServiceAdapter {
+
+  MortgageAccountDetailMockServiceAdapter();
+
+  @override
+  MortgageAccountDetailEntity createEntity(MortgageAccountDetailEntity initialEntity,
+      MortgageAccountDetailServiceResponseModel responseModel) {
+    return MortgageAccountDetailEntity(
+        name: responseModel.name,
+        lastFour: responseModel.lastFour,
+        accountType: responseModel.accountType,
+        routingNumber: responseModel.routingNumber,
+        balance: responseModel.balance,
+        beginningBalance: responseModel.beginningBalance,
+        pendingTransactions: responseModel.pendingTransactions,
+        depositHolds: responseModel.depositHolds
+    );
+  }
+}
--- business_banking/test/features/mortgage_account_detail/api/mortgage_account_detail_service_adapter_test.dart
+++ business_banking/test/features/mortgage_account_detail/api/mortgage_account_detail_service_adapter_test.dart
@@ -3,6 +3,8 @@ import 'package:business_banking/features/mortgage_account_details/bloc/mortgage
 import 'package:business_banking/features/mortgage_account_details/model/mortgage_account_detail_entity.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'mortgage_account_detail_mock_service_adapter.dart';
+
 void main() {
   test('Entity is created by service adapter', () {
     final entity = MortgageAccountDetailServiceAdapter().createEntity(
@@ -28,5 +30,31 @@ void main() {
       pendingTransactions: -228.89,
       depositHolds: 0.00
     ));
+  });
+
+    test('Entity is created by service mock adapter', () {
+      final entity = MortgageAccountDetailMockServiceAdapter().createEntity(
+          MortgageAccountDetailEntity(),
+          MortgageAccountDetailServiceResponseModel.fromJson({
+            'name': 'Mortgage',
+            'lastFour': '6542',
+            'accountType': 'Mortgage',
+            'routingNum': 876395735,
+            'balance': 6735.54,
+            'begBalance': 9243.43,
+            'penTrans': -228.89,
+            'depHolds': 0.00
+          }));
+
+      expect(entity, MortgageAccountDetailEntity(
+          name: 'Mortgage',
+          lastFour: '6542',
+          accountType: 'Mortgage',
+          routingNumber: 876395735,
+          balance: 6735.54,
+          beginningBalance: 9243.43,
+          pendingTransactions: -228.89,
+          depositHolds: 0.00
+      ));
   });
 }
