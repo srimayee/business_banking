b15423544e3f7ac8dc1ecfc1f59d50638e83bbed
Elijah Luckey <luckeyelijah112@gmail.com>
2021-03-05 12:46:39 -0500

    adds coverage variables & options to open lcov

--- business_banking/test_coverage.sh
+++ business_banking/test_coverage.sh
@@ -1,4 +1,14 @@
+#!/usr/bin/env bash
+
+# Flutter and lcov/genhtml, must be installed on you path.
 flutter test --coverage --coverage-path ./coverage/lcov.info test
 genhtml coverage/lcov.info -o coverage/html
+
+reportPath="$(pwd)/coverage/html/index.html"
+
 echo "See the coverage here:"
-echo "$(pwd)/coverage/html/index.html"
+echo $reportPath
+# Uncomment to open automatically:
+# open -a  "Google Chrome" file:///$reportPath
+# open -a  "Firefox" file:///$reportPath
+# open -a  "Safari" file:///$reportPath
