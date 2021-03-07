8672c8ff223cffea6f8bfcc4998c98289b25a408
Alireza Khakpour <1khakpour@gmail.com>
2021-03-06 19:28:18 -0600

    add: Investment entity

---
+++ business_banking/lib/features/investment/model/investment_entity.dart
@@ -0,0 +1,33 @@
+//package
+import 'package:clean_framework/clean_framework.dart';
+
+class InvestmentEntity extends Entity {
+  final double accountBalance;
+  final double totalGainValue;
+  final double totalGainPercent;
+
+  InvestmentEntity({
+    List<EntityFailure> errors = const [],
+    this.accountBalance = 0.00,
+    this.totalGainValue = 0.00,
+    this.totalGainPercent = 0.00,
+  }) : super(errors: errors);
+
+  @override
+  List<Object> get props =>
+      [errors, accountBalance, totalGainValue, totalGainPercent];
+
+  @override
+  merge(
+      {errors,
+      double accountBalance,
+      double totalGainValue,
+      double totalGainPercent}) {
+    return InvestmentEntity(
+      errors: errors ?? this.errors,
+      accountBalance: accountBalance ?? this.accountBalance,
+      totalGainValue: totalGainValue ?? this.totalGainValue,
+      totalGainPercent: totalGainPercent ?? this.totalGainPercent,
+    );
+  }
+}
--- business_banking/lib/locator.dart
+++ business_banking/lib/locator.dart
@@ -25,8 +25,9 @@ class ExampleLocator implements Locator {
 
   UrlLauncher urlLauncher = UrlLauncher();
 
-  PatchSimpleRestApi api = PatchSimpleRestApi(
-      baseUrl: 'http://localhost:3001/'); // Points to Mockoon instance
-
+  // PatchSimpleRestApi api = PatchSimpleRestApi(
+  //     baseUrl: 'http://localhost:3001/'); // Points to Mockoon instance
+  PatchSimpleRestApi api =
+      PatchSimpleRestApi(baseUrl: 'http://192.168.1.64:3001/');
   Repository repository = Repository();
 }
