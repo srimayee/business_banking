b7f565977284883daddfc85a77873b26b012b99f
bikkin4 <bikkin4@nationwide.com>
2021-03-20 18:51:59 -0400

    Added navigation logic for on tap action of QRCode generator

--- business_banking/lib/features/quickpay/ui/quickpay_presenter.dart
+++ business_banking/lib/features/quickpay/ui/quickpay_presenter.dart
@@ -1,25 +1,32 @@
 import 'dart:async';
+
+import 'package:business_banking/features/qr_code_generator/ui/qr_code_generator_widget.dart';
+import 'package:business_banking/features/quickpay/bloc/quickpay_bloc.dart';
+import 'package:business_banking/features/quickpay/model/quickpay_view_model.dart';
+import 'package:business_banking/features/request_money/ui/request_money_widget.dart';
 import 'package:clean_framework/clean_framework.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'quickpay_screen.dart';
-import 'package:business_banking/features/quickpay/bloc/quickpay_bloc.dart';
-import 'package:business_banking/features/quickpay/model/quickpay_view_model.dart';
-import 'package:business_banking/features/request_money/ui/request_money_widget.dart';
 
-class QuickPayPresenter extends Presenter<QuickPayBloc, QuickPayViewModel, QuickPayScreen> {
+class QuickPayPresenter
+    extends Presenter<QuickPayBloc, QuickPayViewModel, QuickPayScreen> {
   @override
   Stream<QuickPayViewModel> getViewModelStream(QuickPayBloc bloc) {
     return bloc.quickPayViewModelPipe.receive;
   }
 
   @override
-  QuickPayScreen buildScreen(BuildContext context, QuickPayBloc bloc, QuickPayViewModel viewModel) {
+  QuickPayScreen buildScreen(
+      BuildContext context, QuickPayBloc bloc, QuickPayViewModel viewModel) {
     return QuickPayScreen(
       viewModel: viewModel,
       navigateToMakePayment: () {
         _navigateToMakePayment(context);
+      },
+      navigateToGenerateQrCode: () {
+        _navigateToGenerateQrCode(context);
       },
     );
   }
@@ -28,9 +35,17 @@ class QuickPayPresenter extends Presenter<QuickPayBloc, QuickPayViewModel, Quick
     Navigator.push(
       context,
       MaterialPageRoute(
-        settings: RouteSettings(name: 'request_money'),
-        builder: (context) => RequestMoneyWidget()
-      ),
+          settings: RouteSettings(name: 'request_money'),
+          builder: (context) => RequestMoneyWidget()),
+    );
+  }
+
+  void _navigateToGenerateQrCode(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          settings: RouteSettings(name: 'seed'),
+          builder: (context) => QRCodeGeneratorWidget()),
     );
   }
 }
