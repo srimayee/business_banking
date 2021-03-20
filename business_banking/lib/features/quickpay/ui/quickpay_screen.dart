d933504a41afc25a164b17a1d49668a8dd706415
bikkin4 <bikkin4@nationwide.com>
2021-03-20 18:50:24 -0400

    add ListTile and navigation action for QRCode generator as asked

--- business_banking/lib/features/quickpay/ui/quickpay_screen.dart
+++ business_banking/lib/features/quickpay/ui/quickpay_screen.dart
@@ -1,50 +1,57 @@
 import 'dart:ui';
-import 'package:auto_size_text/auto_size_text.dart';
+
+import 'package:business_banking/features/quickpay/model/quickpay_view_model.dart';
 import 'package:clean_framework/clean_framework.dart';
 import 'package:flutter/material.dart';
-import 'package:business_banking/features/quickpay/model/quickpay_view_model.dart';
 
 class QuickPayScreen extends Screen {
   final QuickPayViewModel viewModel;
   final VoidCallback navigateToMakePayment;
+  final VoidCallback navigateToGenerateQrCode;
 
   QuickPayScreen(
-      {@required this.viewModel, @required this.navigateToMakePayment})
+      {@required this.viewModel,
+      @required this.navigateToMakePayment,
+      @required this.navigateToGenerateQrCode})
       : assert(() {
-    return viewModel != null;
-  }());
+          return viewModel != null;
+        }());
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200,
-      padding: EdgeInsets.all(5.0),
-      child: Card(
-        child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-          child:Column(children: <Widget>[
-              Expanded(
+        height: 300,
+        padding: EdgeInsets.all(5.0),
+        child: Card(
+            child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+          child: Column(children: <Widget>[
+            Expanded(
                 child: ListTile(
-                  title: Text('Name'),
-                  subtitle:
-                  Text(viewModel.name),
-                  )
-              ),
-                Expanded(
-                  child: ListTile(
-                    title: Text('Email'),
-                    subtitle: Text(viewModel.email),
-                  )
-                ),
-                  RaisedButton(
-                    child: Text("Request Money"),
-                    onPressed: () {
-                      navigateToMakePayment();
-                    },
-                  )
-              ])
-        ),
-      ),
-    );
+              title: Text('Name'),
+              subtitle: Text(viewModel.name),
+            )),
+            Expanded(
+                child: ListTile(
+              title: Text('Email'),
+              subtitle: Text(viewModel.email),
+            )),
+            Expanded(
+                child: ListTile(
+                    title: Text('QRCode'),
+                    trailing: RaisedButton(
+                      child: Text("Generate"),
+                      onPressed: () {
+                        navigateToGenerateQrCode();
+                      },
+                    ))),
+            RaisedButton(
+              child: Text("Request Money"),
+              onPressed: () {
+                navigateToMakePayment();
+              },
+            ),
+          ]),
+        )));
   }
 }
