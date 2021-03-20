5c9d9f7e58eef8828c6bcdc02789e13531fa710f
bikkin4 <bikkin4@nationwide.com>
2021-03-20 18:45:49 -0400

    added bloc implementation for QRCode generator bloc

---
+++ business_banking/test/features/qr_code_generator/bloc/qr_code_generator_bloc_mock.dart
@@ -0,0 +1,22 @@
+import 'package:business_banking/features/qr_code_generator/bloc/qr_code_generator_bloc.dart';
+import 'package:business_banking/features/qr_code_generator/model/qr_code_generator_view_model.dart';
+import 'package:clean_framework/clean_framework.dart';
+import 'package:mockito/mockito.dart';
+
+class QRCodeGeneratorBlocFailureMock extends Fake
+    implements QRCodeGeneratorBloc {
+  Pipe<QRCodeGeneratorViewModel> qrCodeGeneratorViewModelPipe =
+      Pipe<QRCodeGeneratorViewModel>();
+
+  QRCodeGeneratorBlocFailureMock() {
+    qrCodeGeneratorViewModelPipe.whenListenedDo(() {
+      qrCodeGeneratorViewModelPipe.send(QRCodeGeneratorViewModel(
+        seed: '',
+        expiresAt: '',
+      ));
+    });
+  }
+
+  @override
+  void dispose() {}
+}
