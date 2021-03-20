9595e1ca5c5988da3bf367c338e266f309ec9742
bikkin4 <bikkin4@nationwide.com>
2021-03-20 18:42:23 -0400

    Add QrCode generator get request service

---
+++ business_banking/lib/features/qr_code_generator/api/qr_code_generator_service.dart
@@ -0,0 +1,22 @@
+import 'package:business_banking/features/qr_code_generator/api/qr_code_generator_service_model.dart';
+import 'package:business_banking/locator.dart';
+import 'package:clean_framework/clean_framework.dart';
+import 'package:clean_framework/clean_framework_defaults.dart';
+
+class QRCodeGeneratorService extends EitherService<JsonRequestModel,
+    QRCodeGeneratorServiceResponseModel> {
+  QRCodeGeneratorService()
+      : super(
+            method: RestMethod.get,
+            restApi: mockService ?? false
+                ? ExampleLocator().api
+                : ExampleLocator().qrCodeSeedApi,
+            path: 'seed');
+  static bool mockService;
+
+  @override
+  QRCodeGeneratorServiceResponseModel parseResponse(
+      Map<String, dynamic> jsonResponse) {
+    return QRCodeGeneratorServiceResponseModel.fromJson(jsonResponse);
+  }
+}
