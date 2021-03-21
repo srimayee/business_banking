2dc97e161a1276113c325b69815f2ca763cc17a0
bikkinanagadivya09@gmail.com <bikkin4@nationwide.com>
2021-03-21 10:55:18 -0400

    revert back to original

--- business_banking/lib/features/quickpay/ui/quickpay_screen.dart
+++ business_banking/lib/features/quickpay/ui/quickpay_screen.dart
@@ -8,39 +8,40 @@ class QuickPayScreen extends Screen {
   final QuickPayViewModel viewModel;
   final VoidCallback navigateToMakePayment;
 
-  QuickPayScreen({
-    @required this.viewModel,
-    @required this.navigateToMakePayment,
-  }) : assert(() {
+  QuickPayScreen(
+      {@required this.viewModel, @required this.navigateToMakePayment})
+      : assert(() {
           return viewModel != null;
         }());
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 300,
-        padding: EdgeInsets.all(5.0),
-        child: Card(
-            child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-          child: Column(children: <Widget>[
-            Expanded(
-                child: ListTile(
-              title: Text('Name'),
-              subtitle: Text(viewModel.name),
-            )),
-            Expanded(
-                child: ListTile(
-              title: Text('Email'),
-              subtitle: Text(viewModel.email),
-            )),
-            RaisedButton(
-              child: Text("Request Money"),
-              onPressed: () {
-                navigateToMakePayment();
-              },
-            ),
-          ]),
-        )));
+      height: 200,
+      padding: EdgeInsets.all(5.0),
+      child: Card(
+        child: Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+            child: Column(children: <Widget>[
+              Expanded(
+                  child: ListTile(
+                title: Text('Name'),
+                subtitle: Text(viewModel.name),
+              )),
+              Expanded(
+                  child: ListTile(
+                title: Text('Email'),
+                subtitle: Text(viewModel.email),
+              )),
+              RaisedButton(
+                child: Text("Request Money"),
+                onPressed: () {
+                  navigateToMakePayment();
+                },
+              )
+            ])),
+      ),
+    );
   }
 }
