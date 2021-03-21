09a5143791202b571aed6ecc74d87c5a324c41f9
bikkinanagadivya09@gmail.com <bikkin4@nationwide.com>
2021-03-21 11:04:26 -0400

    remove unused imports

--- business_banking/test/features/qr_code_generator/ui/count_down_timer_test.dart
+++ business_banking/test/features/qr_code_generator/ui/count_down_timer_test.dart
@@ -1,11 +1,6 @@
-import 'dart:async';
-
 import 'package:business_banking/features/qr_code_generator/ui/count_down_timer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
-
-
 
 void main() {
   Widget buildTestableWidget(Widget widget) {
