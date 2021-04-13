3355d846386ecff4011360cc73a3863413871523
denismotta <denismotta@gmail.com>
2021-04-12 21:52:45 -0700

    Increases test coverage for PieChart Widget and Presenter

--- business_banking/test/features/transaction/ui/transaction_chart/transaction_chart_ui_test.dart
+++ business_banking/test/features/transaction/ui/transaction_chart/transaction_chart_ui_test.dart
@@ -1,9 +1,12 @@
 import 'package:business_banking/features/transaction/bloc/transaction_bloc.dart';
+import 'package:business_banking/features/transaction/model/transaction_model.dart';
 import 'package:business_banking/features/transaction/model/transaction_view_model.dart';
 import 'package:business_banking/features/transaction/ui/transaction_chart/transaction_chart_presenter.dart';
 import 'package:business_banking/features/transaction/ui/transaction_chart/transaction_chart_screen.dart';
+import 'package:business_banking/features/transaction/ui/transaction_chart/transaction_chart_widget.dart';
 import 'package:business_banking/features/transaction/ui/transaction_presenter.dart';
 import 'package:clean_framework/clean_framework.dart';
+import 'package:enum_to_string/enum_to_string.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -24,5 +27,19 @@ void main() {
     await tester.pump(Duration(milliseconds: 500));
 
     expect(find.byType(TransactionChartScreen), findsOneWidget);
+  });
+
+  testWidgets('Transactions, with true bloc', (tester) async {
+    final testWidget = MaterialApp(
+      home: BlocProvider<TransactionBloc>(
+        create: (_) => TransactionBloc(),
+        child: TransactionChartWidget(),
+        ),
+      );
+
+    await tester.pumpWidget(testWidget);
+    await tester.pump(Duration(milliseconds: 500));
+
+    expect(find.byType(TransactionChartWidget), findsOneWidget);
   });
 }
