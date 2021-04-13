e0796275b668585dfb24c275c5268bf517490968
denismotta <denismotta@gmail.com>
2021-04-12 19:19:22 -0700

    Adds test coverage to piechart screen

---
+++ business_banking/test/features/transaction/ui/transaction_chart/transaction_chart_ui_test.dart
@@ -0,0 +1,28 @@
+import 'package:business_banking/features/transaction/bloc/transaction_bloc.dart';
+import 'package:business_banking/features/transaction/model/transaction_view_model.dart';
+import 'package:business_banking/features/transaction/ui/transaction_chart/transaction_chart_presenter.dart';
+import 'package:business_banking/features/transaction/ui/transaction_chart/transaction_chart_screen.dart';
+import 'package:business_banking/features/transaction/ui/transaction_presenter.dart';
+import 'package:clean_framework/clean_framework.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Transactions, with true bloc', (tester) async {
+    navigateToHubScreen() {}
+    final testWidget = MaterialApp(
+      home: BlocProvider<TransactionBloc>(
+        create: (_) => TransactionBloc(),
+        child: TransactionChartScreen(
+          viewModel: TransactionViewModel(transactionDetails: []),
+          navigateToHubScreen: navigateToHubScreen(),
+        ),
+      ),
+    );
+
+    await tester.pumpWidget(testWidget);
+    await tester.pump(Duration(milliseconds: 500));
+
+    expect(find.byType(TransactionChartScreen), findsOneWidget);
+  });
+}
