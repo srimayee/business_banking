1fcda2ad7c5fe521a4e6917b83e91659ecb13801
Prabhu.Somasundaram <prabhus@virtusa.com>
2021-04-16 08:37:00 -0400

    added transactions list and a bottom sheet to view chart screen

--- business_banking/lib/features/budget/model/budget_entity.dart
+++ business_banking/lib/features/budget/model/budget_entity.dart
@@ -6,13 +6,16 @@ import 'account_info.dart';
 class BudgetEntity extends Entity {
   final AccountInfo? accountInfo;
   final List<PostedTransactions>? allTransactions;
+  final List<PostedTransactions>? filteredTransactions;
 
   BudgetEntity(
       {List<EntityFailure> errors = const [],
       List<PostedTransactions>? allTransactions,
+        List<PostedTransactions>? filteredTransactions,
       AccountInfo? accountInfo})
       : accountInfo = accountInfo,
         allTransactions = allTransactions ?? [],
+        filteredTransactions = filteredTransactions ?? [],
         super(errors: errors);
 
   // @override
@@ -23,11 +26,17 @@ class BudgetEntity extends Entity {
     errors,
     AccountInfo? accountInfo,
     List<PostedTransactions>? allTransactions,
+    List<PostedTransactions>? filteredTransactions,
   }) {
     return BudgetEntity(
       errors: errors ?? this.errors,
       accountInfo: accountInfo ?? this.accountInfo,
       allTransactions: allTransactions ?? this.allTransactions,
+      filteredTransactions: allTransactions ?? this.filteredTransactions,
     );
+  }
+
+  List<PostedTransactions> filterWith(String category) {
+    return [allTransactions!.first];
   }
 }
--- business_banking/lib/features/budget/ui/view_chart_screen.dart
+++ business_banking/lib/features/budget/ui/view_chart_screen.dart
@@ -5,12 +5,19 @@ import 'package:flutter/material.dart';
 
 class ViewChartScreen extends Screen {
   final BudgetViewModel viewModel;
+  final _scaffoldkey = new GlobalKey<ScaffoldState>();
 
   ViewChartScreen({required this.viewModel});
 
   @override
   Widget build(BuildContext context) {
+    List<dynamic> categoryList = viewModel.allTransactions
+        .map((array) => array.category)
+        .toSet()
+        .toList();
+
     return Scaffold(
+      key: _scaffoldkey,
       appBar: AppBar(
         title: Text(viewModel.accountInfo!.accountNickname),
         backgroundColor: Colors.green,
@@ -19,19 +26,103 @@ class ViewChartScreen extends Screen {
         children: [
           Container(
             padding:
-            const EdgeInsets.symmetric(vertical: 18.0, horizontal: 20.0),
+                const EdgeInsets.symmetric(vertical: 18.0, horizontal: 20.0),
             child: Center(
                 child: Text(
-                  'Your Spendings',
-                  style: TextStyle(fontSize: 18),
-                )),
+              'Your Spendings',
+              style: TextStyle(fontSize: 18),
+            )),
           ),
           ConstrainedBox(
-              constraints: BoxConstraints.loose(Size(
+            constraints: BoxConstraints.loose(
+              Size(
                 double.infinity,
                 MediaQuery.of(context).size.width / 1.8,
-              )),
-              child: DonutAutoLabelChart.withSampleData()),
+              ),
+            ),
+            child: DonutAutoLabelChart.withSampleData(),
+          ),
+          Divider(),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.4,
+            child: Card(
+              elevation: 0.0,
+              child: ListView(
+                children: <Widget>[
+                  ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    padding: EdgeInsets.symmetric(horizontal: 5.0),
+                    shrinkWrap: true,
+                    itemCount: viewModel.allTransactions.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Padding(
+                        padding: EdgeInsets.all(5.0),
+                        child: Container(
+                          height: 30.0,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                  viewModel.allTransactions[index].description),
+                              Text('\$' +
+                                  viewModel.allTransactions[index].debitAmount
+                                      .toString()),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+          TextButton(
+            child: Text('Select Category'),
+            onPressed: () {
+              _scaffoldkey.currentState!.showBottomSheet(
+                (context) {
+                  return BottomSheet(
+                    onClosing: () {},
+                    builder: (context) {
+                      return Container(
+                        height: categoryList.length * 70.0,
+                        color: Colors.lightGreen,
+                        child: ListView(
+                          children: <Widget>[
+                            ButtonBar(
+                              children: <Widget>[
+                                CloseButton()],
+                            ),
+                            ListView.builder(
+                              physics: NeverScrollableScrollPhysics(),
+                              padding: EdgeInsets.symmetric(horizontal: 5.0),
+                              shrinkWrap: true,
+                              itemCount: categoryList.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                return InkWell(
+                                  onTap: () => {},
+                                  child: Center(
+                                    child: Container(
+                                      height: 50.0,
+                                      child: Text(
+                                        categoryList[index],
+                                        style: TextStyle(fontSize: 14.0),
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                },
+              );
+            },
+          ),
         ],
       ),
     );
