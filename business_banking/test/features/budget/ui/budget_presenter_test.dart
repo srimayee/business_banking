02c01365f018793596b03321410f28f9af232e18
Prabhu.Somasundaram <prabhus@virtusa.com>
2021-04-21 11:21:55 -0400

    added test cases to test presenter

---
+++ business_banking/test/features/budget/ui/budget_presenter_test.dart
@@ -0,0 +1,50 @@
+// @dart=2.9
+
+import 'package:business_banking/features/budget/bloc/budget_bloc.dart';
+import 'package:business_banking/features/budget/ui/chart/view_chart_presenter.dart';
+import 'package:business_banking/features/budget/ui/chart/view_chart_widget.dart';
+import 'package:business_banking/features/budget/ui/first_card/budget_presenter.dart';
+import 'package:business_banking/features/budget/ui/first_card/budget_widget.dart';
+import 'package:business_banking/features/budget/ui/transactions_list/list_transactions_presenter.dart';
+import 'package:business_banking/features/budget/ui/transactions_list/list_transactions_widget.dart';
+import 'package:clean_framework/clean_framework.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('UI TEST: should add BudgetPresenter to widget tree', (tester) async {
+    final testWidget = MaterialApp(
+      home: BlocProvider<BudgetBloc>(
+          child: BudgetWidget(), create: (_) => BudgetBloc()),
+    );
+
+    await tester.pumpWidget(testWidget);
+    await tester.pumpAndSettle();
+
+    expect(find.byType(BudgetPresenter), findsOneWidget);
+  });
+
+  testWidgets('UI TEST: should add ViewChartPresenter to widget tree', (tester) async {
+    final testWidget = MaterialApp(
+      home: BlocProvider<BudgetBloc>(
+          child: ViewChartWidget(), create: (_) => BudgetBloc()),
+    );
+
+    await tester.pumpWidget(testWidget);
+    await tester.pumpAndSettle();
+
+    expect(find.byType(ViewChartPresenter), findsOneWidget);
+  });
+
+  testWidgets('UI TEST: should add ListTransactionsPresenter to widget tree', (tester) async {
+    final testWidget = MaterialApp(
+      home: BlocProvider<BudgetBloc>(
+          child: ListTransactionsWidget(), create: (_) => BudgetBloc()),
+    );
+
+    await tester.pumpWidget(testWidget);
+    await tester.pumpAndSettle();
+
+    expect(find.byType(ListTransactionsPresenter), findsOneWidget);
+  });
+}
