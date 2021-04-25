ece1e597396167af34f6c4df79f2673662f90612
Dan Shriver <dshriver53@gmail.com>
2021-04-25 16:50:11 -0500

    reworked StocksEntity and tests

--- business_banking/lib/features/stocks/model/stocks_entity.dart
+++ business_banking/lib/features/stocks/model/stocks_entity.dart
@@ -1,23 +1,32 @@
 import 'package:clean_framework/clean_framework.dart';
 
 class StocksEntity extends Entity {
-  final String company;
-  final String symbol;
-  final double atClose;
-  final int shares;
-  final double value;
+  final List<String> stocks;
+  final double totalValue;
 
-  StocksEntity(
-      {List<EntityFailure> errors = const [],
-      String? company,
-      String? symbol,
-      this.atClose = 0.0,
-      this.shares = 0,
-      this.value = 0.0})
-      : this.company = company ?? "",
-        this.symbol = symbol ?? "",
-        super(errors: errors);
+  StocksEntity({List<String>? stocks, this.totalValue = 0.0})
+      : this.stocks = stocks ?? [];
 
   @override
-  List<Object> get props => [errors, company, symbol, atClose, shares, value];
+  List<Object> get props => [errors, stocks, totalValue];
+ 
+  // final String company;
+  // final String symbol;
+  // final double atClose;
+  // final int shares;
+  // final double value;
+
+  // StocksEntity(
+  //     {List<EntityFailure> errors = const [],
+  //     String? company,
+  //     String? symbol,
+  //     this.atClose = 0.0,
+  //     this.shares = 0,
+  //     this.value = 0.0})
+  //     : this.company = company ?? "",
+  //       this.symbol = symbol ?? "",
+  //       super(errors: errors);
+
+  // @override
+  // List<Object> get props => [errors, company, symbol, atClose, shares, value];
 }
--- business_banking/test/features/stocks/model/stocks_entity_test.dart
+++ business_banking/test/features/stocks/model/stocks_entity_test.dart
@@ -3,25 +3,18 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:business_banking/features/stocks/model/stocks_entity.dart';
 
 void main() {
-  StocksEntity entity = StocksEntity(
-      company: "Test", symbol: "TEST", atClose: 1, shares: 100, value: 100);
+  StocksEntity entity = StocksEntity(stocks: ["test"], totalValue: 0.0);
 
   test('StockEntity properties', () {
-    expect(entity.company, "Test");
-    expect(entity.symbol, "TEST");
-    expect(entity.atClose, 1);
-    expect(entity.shares, 100);
-    expect(entity.value, 100);
+    expect(entity.stocks, ["test"]);
+    expect(entity.totalValue, 0.0);
 
-    expect(entity.toString(), 'StocksEntity([], Test, TEST, 1.0, 100, 100.0)');
+    expect(entity.toString(), 'StocksEntity([], [test], 0.0)');
   });
 
   test('StocksEntity merge with errors = null', () {
     entity.merge(errors: null);
-    expect(entity.company, "Test");
-    expect(entity.symbol, "TEST");
-    expect(entity.atClose, 1);
-    expect(entity.shares, 100);
-    expect(entity.value, 100);
+    expect(entity.stocks, ["test"]);
+    expect(entity.totalValue, 0.0);
   });
 }
