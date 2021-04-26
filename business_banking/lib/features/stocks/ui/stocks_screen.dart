0cdb7d4407b32186ade6f6e0f0c1e618598f7826
Dan Shriver <dshriver53@gmail.com>
2021-04-26 14:40:39 -0500

    Created Stocks presenter, screen, widget

---
+++ business_banking/lib/features/stocks/ui/stocks_screen.dart
@@ -0,0 +1,17 @@
+import 'package:business_banking/features/stocks/model/stocks_view_model.dart';
+import 'package:clean_framework/clean_framework.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class StocksScreen extends Screen {
+  final StocksViewModel viewModel;
+
+  StocksScreen({required this.viewModel});
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Center(child: Text('Stocks'),),);
+  }
+  
+}
