4491553199fb0ae3d8d32081ca7cb05f033be814
Dan Shriver <dshriver53@gmail.com>
2021-04-28 17:26:31 -0500

    fixed stocks service test

--- business_banking/test/features/stocks/api/stocks_service_test.dart
+++ business_banking/test/features/stocks/api/stocks_service_test.dart
@@ -19,6 +19,41 @@ void main() {
             "at_close": 89.38,
             "shares": 100,
             "value": 8938.0
+          },
+          {
+            "company": "Disney",
+            "symbol": "DIS",
+            "at_close": 40.65,
+            "shares": 100,
+            "value": 4065.00
+          },
+          {
+            "company": "PepsiCo",
+            "symbol": "PEP",
+            "at_close": 38.02,
+            "shares": 100,
+            "value": 3802.00
+          },
+          {
+            "company": "Apple",
+            "symbol": "AAPL",
+            "at_close": 134.22,
+            "shares": 100,
+            "value": 13422.00
+          },
+          {
+            "company": "Home Depot",
+            "symbol": "HD",
+            "at_close": 323.60,
+            "shares": 100,
+            "value": 32360.00
+          },
+          {
+            "company": "Nike",
+            "symbol": "NIKE",
+            "at_close": 130.19,
+            "shares": 100,
+            "value": 13019.00
           }
         ]
       }),
