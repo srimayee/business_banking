3c25b1ea63112525cc733abf746d36866ffee47f
bikkinanagadivya <bikkinanagadivya09@gmail.com>
2021-05-06 23:42:17 -0400

    create registration screen

--- business_banking/lib/features/hub/ui/hub_screen.dart
+++ business_banking/lib/features/hub/ui/hub_screen.dart
@@ -1,6 +1,4 @@
-import 'package:business_banking/features/budget/ui/first_card/budget_widget.dart';
-import 'package:business_banking/features/credit_card/ui/credit_card/credit_card_widget.dart';
-import 'package:business_banking/features/deposit_check/ui/1st_hub_card/deposit_check_card_widget.dart';
+import 'package:business_banking/features/online_registration/ui/online_registration_feature_widget.dart';
 import 'package:clean_framework/clean_framework.dart';
 import 'package:flutter/material.dart';
 
@@ -18,24 +16,7 @@ class HubScreen extends Screen {
         ),
       ),
       backgroundColor: Colors.grey[300],
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 18.0, horizontal: 20.0),
-              child: Center(
-                  child: Text(
-                'Welcome John Doe',
-                style: TextStyle(fontSize: 18),
-              )),
-            ),
-            CreditCardWidget(),
-            BudgetWidget(),
-            DepositCheckCardWidget(),
-          ],
-        ),
-      ),
+      body: OnlineRegistrationFeatureWidget(),
     );
   }
 }
