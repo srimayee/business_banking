1f0d4c00492231bc8cfccee06a7a127c2f927e3a
bikkinanagadivya <bikkinanagadivya09@gmail.com>
2021-05-06 23:48:33 -0400

    create post call service for online registration screen form

---
+++ business_banking/lib/features/online_registration/api/online_registration_service.dart
@@ -0,0 +1,21 @@
+import 'package:business_banking/features/online_registration/api/online_registration_service_request_model.dart';
+import 'package:business_banking/features/online_registration/api/online_registration_service_response_model.dart';
+import 'package:business_banking/locator.dart';
+import 'package:clean_framework/clean_framework.dart';
+import 'package:clean_framework/clean_framework_defaults.dart';
+
+class OnlineRegistrationService extends EitherService<
+    OnlineRegistrationServiceRequestModel,
+    OnlineRegistrationServiceResponseModel> {
+  OnlineRegistrationService()
+      : super(
+            method: RestMethod.post,
+            restApi: ExampleLocator().api,
+            path: 'enter_path_here_once_ready');
+
+  @override
+  OnlineRegistrationServiceResponseModel parseResponse(
+      Map<String, dynamic> jsonResponse) {
+    return OnlineRegistrationServiceResponseModel.fromJson(jsonResponse);
+  }
+}
