33b021c595e8de4e1dc9913afa11bff38f1bccbc
potatology <strigunovvladimir@gmail.com>
2021-05-24 19:46:12 -0700

    dependencies in pubspec and Mockoon env

--- business_banking/lib/features/credit_card/ui/payment_response/credit_card_payment_response_actions.dart
+++ business_banking/lib/features/credit_card/ui/payment_response/credit_card_payment_response_actions.dart
@@ -3,24 +3,22 @@ import 'package:business_banking/features/credit_card/model/payment_response/cre
 import 'package:clean_framework/clean_framework.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:pdf/widgets.dart' as pw;
-import 'package:printing/printing.dart';
 
 import '../../../../routes.dart';
 
 class CreditCardPaymentResponseActions {
-
   final CreditCardBloc bloc;
   final CreditCardPaymentResponseViewModel viewModel;
 
   CreditCardPaymentResponseActions(this.bloc, this.viewModel);
 
   void navigateBack(BuildContext context) {
-     CFRouterScope.of(context).popUntil(BusinessBankingRouter.creditCardDetailsRoute);
+    CFRouterScope.of(context)
+        .popUntil(BusinessBankingRouter.creditCardDetailsRoute);
   }
 
   void sharePDFPaymentConfirmation(BuildContext context) async {
     pw.Document pdf = await bloc.generatePDFPaymentConfirmation(viewModel);
-    Printing.sharePdf(bytes: await pdf.save(), filename: 'payment-confirmation.pdf');
+    //Printing.sharePdf(bytes: await pdf.save(), filename: 'payment-confirmation.pdf');
   }
-
 }
