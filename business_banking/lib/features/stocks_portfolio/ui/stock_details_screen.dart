bbc55ac8b3d4a0736980cbb804c0376eb873d116
dan461 <dshriver53@gmail.com>
2021-06-01 13:28:56 -0500

    stock details screen ui updates

--- business_banking/lib/features/stocks_portfolio/ui/stock_details_screen.dart
+++ business_banking/lib/features/stocks_portfolio/ui/stock_details_screen.dart
@@ -15,20 +15,57 @@ class StockDetailsScreen extends Screen {
         backgroundColor: Colors.green,
         title: Text(viewModel.company),
       ),
-      body: Column(
-        children: [
-          Row(
-            key: Key('stock-details-chart'),
-            children: [Text('chart')],
-          ),
-          Row(
-            children: [
-              Column(
-                children: [Text(viewModel.company)],
-              )
-            ],
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(viewModel.symbol,
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 4, 16, 4),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    viewModel.company,
+                    style: TextStyle(fontSize: 14),
+                  ),
+                  Text(viewModel.value.toString(),
+                      style: TextStyle(fontSize: 14))
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 4, 0, 4),
+              child: Container(
+                color: Colors.grey[100],
+                height: 200,
+                child: Row(
+                  key: Key('stock-details-chart'),
+                  children: [Text('chart')],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 4, 4, 4),
+              child: Text('Volume: ' + viewModel.volume),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 4, 4, 4),
+              child: Text('Open: ' + viewModel.atOpen.toString()),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 4, 4, 4),
+              child: Text('High: ' + viewModel.highValue.toString()),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 4, 4, 4),
+              child: Text('Low: ' + viewModel.lowValue.toString()),
+            ),
+            // Text()
+          ],
+        ),
       ),
     );
   }
